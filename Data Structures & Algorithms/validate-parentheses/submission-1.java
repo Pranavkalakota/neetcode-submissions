10fde98027db8438dcdb8eb0af63c35d594e839d
Pranavkalakota <pranavkalakota3@gmail.com>
2026-08-26 11:32:21 -0400

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        // convert the string to a character array, so that we can get each element by itself.
+        // push every opening character to the stack
+        // want to compare it with the most recent character (stack)
+
+        if (s.length() % 2 != 0) {
+            // odd number of characters, no pair exists. 
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == '[') {
+                stack.push(c);
+            } else if (c == '{') {
+                stack.push(c);
+            } else {
+                char topChar = stack.pop();
+                // ensures matching
+                if (c == '}' && topChar != '{') {
+                    return false;
+                } else if (c == ']' && topChar != '[') {
+                    return false;
+                } else if (c == ')' && topChar != '(') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
