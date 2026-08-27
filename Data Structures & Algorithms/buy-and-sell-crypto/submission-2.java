0e2f32d7a719c06d5bd1a1914ede3d5840b74b76
Pranavkalakota <pranavkalakota3@gmail.com>
2026-08-27 10:35:12 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buyPointer = 0;
+        int sellPointer = 1; // sell in a different day
+        int[] profitsArray = new int[prices.length - 1];
+        while (sellPointer < prices.length && prices.length >= 2) {
+            if (prices[buyPointer] > prices[sellPointer]) {
+                buyPointer = sellPointer; // finds the correct location for buyPointer
+            } 
+            int difference = prices[sellPointer] - prices[buyPointer];
+            profitsArray[sellPointer - 1] = difference;
+            sellPointer++;
+        }
+        int maxProfit = profitsArray[0];
+        for (int i = 1; i < profitsArray.length; i++) {
+            if (profitsArray[i] > maxProfit) {
+                maxProfit = profitsArray[i];
+            }
+        }
+        return maxProfit;
+    }
+}
