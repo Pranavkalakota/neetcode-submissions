1839e72b0bffd28b1fa6f7a92714e84fb65b1ae9
Pranavkalakota <pranavkalakota3@gmail.com>
2026-09-02 00:03:04 -0400

    Add: is-subsequence - submission-4

---
+++ Data Structures & Algorithms/is-subsequence/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        /* 1) check every letter of s with every letter of t
+           2) if s == t, then move onto the next letter in s. 
+
+        */
+        int sPointer = 0;
+        int tPointer = 0;
+        while (sPointer < s.length() && tPointer < t.length()) {
+            if (s.charAt(sPointer) == t.charAt(tPointer)) {
+                sPointer++;
+            }
+            tPointer++; // if sPointer doesn't match, just move tPointer
+        }
+        if (sPointer == s.length() - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
